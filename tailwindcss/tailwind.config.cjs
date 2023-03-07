1ce86023d92b8b3ccb7743f5e45928c812ffe76a
Reinaldo Duque <reinaldodu@gmail.com>
2023-03-07 10:33:32 -0500

    Iniciando con TailwindCSS

--- tailwindcss/tailwind.config.cjs
+++ tailwindcss/tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fondo': '#EFE7E3',
+        'texto-menu': '#1B2126',
+        'texto-boton': '#DFD0AE',
+        'fondo-boton': '#000000',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
